0c0497c73c36380426589ee4f301ac08a18a653c
Kevin Kayisire <kaykevin1@gmail.com>
2024-07-12 12:34:25 +0000

    Minor bug fix

--- src/shared/config/database.config.ts
+++ src/shared/config/database.config.ts
@@ -5,11 +5,11 @@ require('dotenv').config();
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: '127.0.0.1',
-  port: 3306,
-  database: 'tike',
-  username: 'root',
-  password: 'Benitha.devops123',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/src/db/migrations/*.js'],
   migrationsTableName: 'migrations',
