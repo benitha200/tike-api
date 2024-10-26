9d40152d51a7ec90576e6e56e87165e9a3dd6482
Louange <benithaiyuyisenga2002@gmail.com>
2024-10-26 17:01:16 +0200

    changes

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -489,7 +489,7 @@ export class PaymentService {
         
         console.error('Payment processing error:', error);
         throw new HttpException(
-          `Payment processing failed. Please try again later. ${this.configService.get<string>('INTOUCH_API_URL')}  ${paymentRequest}`,
+          `Payment processing failed. Please try again later. ${this.configService.get<string>('INTOUCH_API_URL')} ${JSON.stringify(paymentRequest, null, 2)}`,
           HttpStatus.INTERNAL_SERVER_ERROR
         );
       }
