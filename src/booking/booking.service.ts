c27940a7de1a7ea0e77d81da647b12b231b98af6
Louange <benithaiyuyisenga2002@gmail.com>
2024-10-28 19:00:06 +0200

    changes

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -182,70 +182,179 @@
 // booking.service.ts
 import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
 import { Booking } from './entities/booking.entity';
 import { InterceptDto } from '../shared/dto/intercept.dto';
+import { Brackets, DataSource, Repository } from 'typeorm';
+import { Trip } from 'src/trip/entities/trip.entity';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class BookingService {
   constructor(
     @InjectRepository(Booking)
-    private bookingRepository: Repository<Booking>,
+    private readonly bookingRepository: Repository<Booking>,
+    @InjectRepository(Trip)
+    private readonly tripRepository: Repository<Trip>,
+    private dataSource: DataSource,
   ) {}
 
-  async create(createBookingDto: CreateBookingDto): Promise<Booking> {
+  // async create(createBookingDto: CreateBookingDto): Promise<Booking> {
+  //   try {
+  //     const booking = this.bookingRepository.create(createBookingDto);
+  //     return await this.bookingRepository.save(booking);
+  //   } catch (error) {
+  //     throw new BadRequestException(error.message);
+  //   }
+  // }
+
+  // async findAll(traveler: string, intercept: InterceptDto): Promise<Booking[]> {
+  //   try {
+  //     return await this.bookingRepository.find({
+  //       where: { traveler: { id: traveler } },
+  //       relations: ['trip', 'traveler'],
+  //     });
+  //   } catch (error) {
+  //     throw new BadRequestException(error.message);
+  //   }
+  // }
+
+  // async findOne(id: string, intercept: InterceptDto): Promise<Booking> {
+  //   try {
+  //     const booking = await this.bookingRepository.findOne({
+  //       where: { id },
+  //       relations: ['trip', 'traveler'],
+  //     });
+
+  //     if (!booking) {
+  //       throw new NotFoundException(`Booking with ID "${id}" not found`);
+  //     }
+
+  //     return booking;
+  //   } catch (error) {
+  //     if (error instanceof NotFoundException) {
+  //       throw error;
+  //     }
+  //     throw new BadRequestException(error.message);
+  //   }
+  // }
+
+  // async cancel(id: string, intercept: InterceptDto): Promise<string> {
+  //   try {
+  //     const booking = await this.findOne(id, intercept);
+  //     booking.canceled = true;
+  //     await this.bookingRepository.save(booking);
+  //     return 'Booking canceled successfully';
+  //   } catch (error) {
+  //     if (error instanceof NotFoundException) {
+  //       throw error;
+  //     }
+  //     throw new BadRequestException(error.message);
+  //   }
+  // }
+
+
+    async create(payload: CreateBookingDto): Promise<Booking> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
-      const booking = this.bookingRepository.create(createBookingDto);
-      return await this.bookingRepository.save(booking);
+      const trip = await this.tripRepository
+        .createQueryBuilder('trips')
+        .where('trips.id = :id', {
+          id: payload.trip,
+        })
+        .getOne();
+      const price = trip ? trip.price : 0;
+
+      let newBooking = new Booking();
+      newBooking.idempotency_key = payload.idempotency_key;
+      newBooking.is_one_way = payload.is_one_way;
+      newBooking.notes = payload.notes;
+      newBooking.trip = payload.trip;
+      newBooking.price = price;
+      newBooking.traveler = payload.traveler;
+      newBooking = await queryRunner.manager.save(newBooking);
+      await queryRunner.commitTransaction();
+      return newBooking;
     } catch (error) {
-      throw new BadRequestException(error.message);
+      await queryRunner.rollbackTransaction();
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    } finally {
+      await queryRunner.release();
     }
   }
 
   async findAll(traveler: string, intercept: InterceptDto): Promise<Booking[]> {
     try {
-      return await this.bookingRepository.find({
-        where: { traveler: { id: traveler } },
-        relations: ['trip', 'traveler'],
-      });
+      return await this.bookingRepository
+        .createQueryBuilder('bookings')
+        .leftJoinAndSelect('bookings.traveler', 'travelers')
+        .leftJoinAndSelect('bookings.trip', 'trips')
+        .leftJoinAndSelect('trips.departure_location', 'locations as departure')
+        .leftJoinAndSelect('trips.arrival_location', 'locations as arrival')
+        .leftJoinAndSelect('trips.operator', 'operators')
+        .leftJoinAndSelect('trips.car', 'cars')
+        .leftJoinAndSelect('trips.driver', 'drivers')
+        .where(
+          new Brackets((qb) => {
+            if (traveler) {
+              qb.where('travelers.id = :traveler', { traveler });
+            }
+          }),
+        )
+        .getMany();
     } catch (error) {
-      throw new BadRequestException(error.message);
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   async findOne(id: string, intercept: InterceptDto): Promise<Booking> {
     try {
-      const booking = await this.bookingRepository.findOne({
-        where: { id },
-        relations: ['trip', 'traveler'],
-      });
-
-      if (!booking) {
-        throw new NotFoundException(`Booking with ID "${id}" not found`);
-      }
-
-      return booking;
+      return await this.bookingRepository
+        .createQueryBuilder('bookings')
+        .leftJoinAndSelect('bookings.traveler', 'travelers')
+        .leftJoinAndSelect('bookings.trip', 'trips')
+        .leftJoinAndSelect('trips.departure_location', 'locations as departure')
+        .leftJoinAndSelect('trips.arrival_location', 'locations as arrival')
+        .leftJoinAndSelect('trips.operator', 'operators')
+        .leftJoinAndSelect('trips.car', 'cars')
+        .leftJoinAndSelect('trips.driver', 'drivers')
+        .where('bookings.id = :id', {
+          id,
+        })
+        .getOne();
     } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-      throw new BadRequestException(error.message);
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   async cancel(id: string, intercept: InterceptDto): Promise<string> {
     try {
-      const booking = await this.findOne(id, intercept);
-      booking.canceled = true;
-      await this.bookingRepository.save(booking);
-      return 'Booking canceled successfully';
+      await this.bookingRepository
+        .createQueryBuilder()
+        .update(Booking)
+        .set({ canceled: true })
+        .where('id = :id', { id })
+        .execute();
+
+      return 'Canceled Booking successfully!';
     } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-      throw new BadRequestException(error.message);
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
